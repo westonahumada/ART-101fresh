d46db34e5288e7fafb9614633d57eb6a574a2789
westonahumada <westonahumada@gmail.com>
2025-10-29 21:11:08 -0700

    catch up work and lab 5

---
+++ environ/env4.js
@@ -0,0 +1,33 @@
+var day = 0;
+var colors = ["Orange", "Aqua", "Pink", "Grey"];
+
+
+
+let myCharacters = ["three legged dog", "2D chain"];
+let aliveCharacter = {
+    species: "pitbull",
+    dimension: "3D",
+    medium: "paper",
+    friends: ["weston", "the chain"]
+};
+
+let megaText;
+megaText = "<p>The subjects of Chain World are: " + myCharacters[0]+ ", " + myCharacters[1] + "</p>"
+
+megaText = megaText + "<p>My alive subject posseses such attributes and appointments such as: species - " + aliveCharacter.species + ", dimension " + aliveCharacter.dimension + ", friends with: " + aliveCharacter.friends[0] + "</p>";
+
+$("#output").append(megaText);
+
+let count = 0;
+
+$("#needy-button").click(function () {
+    $("body").css("background-color", colors[count]);
+   $("#needy-button").html("Clicks: " + count + " Color: " + colors[count] );
+
+
+    count++;
+
+   if (count === colors.length) {
+        count = 0; 
+    }
+});
